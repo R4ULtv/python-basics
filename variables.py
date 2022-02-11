229d8ca6c9fdccdba4db8e52941fb265befa6bb5
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-11 23:11:53 +0100

    Basics python #1

---
+++ variables.py
@@ -0,0 +1,8 @@
+# TYPE COMMENT
+
+number = 1              # INT TYPE
+string = "Hello World"  # STRING TYPE
+bool = True             # BOOLEAN TYPE {True/False}
+float = 3.14            # FLOAT TYPE 
+
+print(string)           # PRINT FUNCTION {print on one line of text}
