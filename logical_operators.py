d00bb01fcea41d7aec1663081843bdc6e2d39cc2
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-13 21:33:25 +0100

    Basics Python #4

---
+++ logical_operators.py
@@ -0,0 +1,16 @@
+# LOGICAL OPERATORS 
+
+y = 1
+x = 1
+
+if y == x and x == 1:
+    # {Logical AND: True if both the operands are true}
+    print("Example of AND")
+
+if y == x or x == 1:
+    # {Logical OR: True if either of the operands is true}
+    print("Example of OR")
+
+if not y:
+    # {Logical NOT: True if operand is false}
+    print("Example of NOT")
