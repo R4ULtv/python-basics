5b6dd44b3675ebb82c111011952cc3d86ebb7f5e
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-13 21:50:41 +0100

    Basics Python #5

--- loops.py
+++ loops.py
@@ -1,1 +1,9 @@
 # LOOPS FOR AND WHILE
+
+for i in range(0,10,1): # {start,stop,step} {if you write range (10) python automatically uses 0 as start, the step is 1 and we insert the stop}
+    # the code runs a specific number of loops in range(), you can use i as an index
+    print(i)
+
+while True:
+    # {the code will continue to cycle as long as the condition is true}
+    print("Example of While")
