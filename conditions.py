b25055df8d646a5d62e31ebc9d3eb588ff352b09
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-13 19:34:46 +0100

    Basics Python #3

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# CONDITIONS AND IF/ELIF/ELSE
+
+1 < 2       # LESS
+1 > 2       # MAJOR
+1 == 2      # EQUALS
+1 != 2      # DIFFERENT
+
+if 1 < 2:
+    # {if 1 is less than 2 it do this}
+    print()
+elif 2 > 1:
+    # {else if 2 is major than 1 do this}
+    print()
+else:
+    # {else do this}
+    print()
+
+# {if the first condition is not true then do the next one and so on}
