3115570e52c8b8370c94ffa7070974677ee0a114
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-18 18:42:28 +0100

    Basics Python #6

---
+++ data-types.py
@@ -0,0 +1,16 @@
+# DATA TYPE: LIST, TUPLE, SET, DICTIONARIE
+
+list = [1,2,3,4] # List items are ordered, changeable, and allow duplicate values.
+print(list)
+
+tuple = (1,2,3,4) # Tuple items are ordered, unchangeable, and allow duplicate values.
+print(tuple)
+
+set = {1,2,3,4} # Set items are unordered, unchangeable, and do not allow duplicate values.
+print(set)
+
+dictionarie = { #Dictionary items are ordered, changeable, and does not allow duplicates.
+    "num1": "1",
+    "num2": "2"
+}
+print(dictionarie)
