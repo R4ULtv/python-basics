f2b4c4565a7599b07b06d54af2f39c0b9d8ab86e
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-18 19:18:03 +0100

    Basics Python #8

---
+++ strings.py
@@ -0,0 +1,13 @@
+# STRINGS METHODS
+
+string = "     this is a STRING"
+
+print(string.strip()) # The strip() method removes any leading and trailing characters (space is the default leading character to remove)
+
+print(len(string)) # The len() function returns the number of characters in the string.
+
+print(string.lower()) # The lower() method returns a string where all characters are lower case.
+
+print(string.upper()) # The upper() method returns a string where all characters are in upper case.
+
+print(string.split()) #The split() method splits a string into a list. You can specify the separator, default separator is any whitespace.
