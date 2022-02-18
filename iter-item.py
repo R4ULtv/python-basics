9e95e6b50d3bdf2221b2bd73aea50ecc89126c1f
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2022-02-18 18:59:29 +0100

    Basics Python #7

---
+++ iter-item.py
@@ -0,0 +1,11 @@
+# ITERACTION BY ITEM
+
+numbers = [0,1,2,3,4,5]
+
+for number in numbers: # For Loops, number is the index and numbers is the lenght of the loops
+    print(number)
+
+# EXAMPLE
+for number in numbers:
+    if number > 1 and number < 4:
+        print(number)
